3673da39a573a66c4eb8eb5f95ed4ed57f40a40d
jinnic <yoohyojin@gmail.com>
2020-06-04 16:41:47 -0400

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,70 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  get '/artists' do
+
+    @artists = Artist.all #define instance variable for view
+  
+    erb :index #show all artists view (index)
+  end
+
+  get '/artists/new' do
+
+    erb :new #show new artists view
+  
+  end
+
+  get '/artists/:id' do
+
+    #gets params from url
+  
+    @artist = Artist.find(params[:id]) #define instance variable for view
+  
+    erb :show #show single artist view
+  
+  end
+
+  delete '/artists/:id' do
+    binding.pry
+    #get params from url
+    Artist.destroy(params[:id]) #define variable to edit
+    
+    redirect "/artists"
+  
+  end
+
+  patch '/artists/:id' do
+    # binding.pry
+    #get params from url
+    @artist = Artist.find(params[:id]) #define variable to edit
+  
+    @artist.update(params[:artist]) #assign new attributes
+  
+    redirect "/artists/#{@artist.id}"
+  
+  end
+
+  
+
+  post '/artists' do
+    # binding.pry
+    #below works with properly formatted params in HTML form
+    @artist = Artist.create(params[:artist]) #create new artist
+    
+    redirect "/artists/#{@artist.id}"
+  
+  end
+
+  get '/artists/:id/edit' do
+
+    #get params from url
+    @artist = Artist.find(params[:id]) #define intstance variable for view
+  
+    erb :edit #show edit artist view
+  
+  end
+
+  
+  
+
 end
